d525bb14da98ee90c84bc0e6b9ef32ecd10635f4
Apprentice <apprentice@dbc23.local>
2013-09-26 16:25:02 -0700

    added user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe User do 
-  let(:valid_user) {User.new(name: "Richie", age: "25", sex: "male", sex_preference: "women", tagline: "GET SOME" )}
-  
-    it "can create an account" do 
-      valid_user.should be_valid 
-    end
-  
+  let(:valid_user) { User.new(name: "Richie", age: "25", sex: "male", sex_preference: "women", email: "richie@gmail.com", tagline: "GET SOME") }
+   
+      it { should validate_presence_of (:email) }
+      it { should validate_uniqueness_of (:email) }
+      it { should validate_presence_of (:name) }
+      it { should validate_presence_of (:age) }
+      it { should validate_presence_of (:sex) }
+      it { should validate_presence_of (:sex_preference) }
 
 end
