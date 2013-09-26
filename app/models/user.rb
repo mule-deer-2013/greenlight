1ecdaff5273fa4f7c9e2cfd62ca53c6be7196568
Apprentice <apprentice@dbc23.local>
2013-09-26 16:54:53 -0700

    Add attr accessible to model for mass assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessible :name, :sex, :sex_preference, :age, :email, :tagline
   validates :name, :email, :sex, :sex_preference, :age, presence: true
   validates :email, uniqueness: true
 
