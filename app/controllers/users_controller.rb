4060854a73c984f29859e32d6b8a42303d0258d0
Richard Serna <Bigpapa1>
2013-09-28 16:06:13 -0700

    no json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
     @user = User.first
-    render :json => @user.to_json, :callback => params[:callback]
+    # render :json => @user.to_json, :callback => params[:callback]
   end
 
   def show
