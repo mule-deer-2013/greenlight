df2c98f04065228f3ed2c2ef38e7fcd084e632b5
Richard Serna <Bigpapa1>
2013-09-28 15:38:18 -0700

    config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], 
-  	:scope => 'name, photo, email' :display => "popup", :client_options => { :ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  	:scope => 'name, photo, email' :display => "popup"
   	OmniAuth.config.logger = Rails.logger
 end
 
@@ -14,3 +14,5 @@ end
 
 # FACEBOOK_APP_ID: "204922036351905"
 # FACEBOOK_SECRET: ed79eeea6d5f82a9bc5e4c8f84ef2579
+
+# , :client_options => { :ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
