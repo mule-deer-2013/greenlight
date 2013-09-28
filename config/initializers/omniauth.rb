d755ce4795dc191fb58efb913258b4f41464f7f0
Richard Serna <Bigpapa1>
2013-09-28 13:27:41 -0700

    deleted application.yaml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,12 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :display => "popup", {:scope => 'PERMISSION_1, PERMISSION_2, PERMISSION_3...,' :client_options => { :ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :display => "popup"
 end
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :display => "popup", {:scope => 'PERMISSION_1, PERMISSION_2, PERMISSION_3...,' :client_options => { :ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+# end
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
 # 	if Rails.env.development?
@@ -12,3 +16,6 @@ end
 # 	  provider :facebook, 'DEPLOY_APP_ID', 'DEPLOY_APP_SECRET'
 #   end
 # end
+
+# FACEBOOK_APP_ID: "204922036351905"
+# FACEBOOK_SECRET: "ed79eeea6d5f82a9bc5e4c8f84ef2579"
