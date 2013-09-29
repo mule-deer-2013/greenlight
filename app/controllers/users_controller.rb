1775ec950315201cc257b3a1d5c52e6220eaf6f3
Richard Serna <Bigpapa1>
2013-09-28 21:39:03 -0700

    fixed error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def update
   	user = User.find(params[:id])
-    user.update_attributes(sex_preference: params[:sex_preference, tagline: params[:tagline], sex: params[:sex])
+    user.update_attributes(sex_preference: params[:sex_preference], tagline: params[:tagline], sex: params[:sex])
     render :json => user.to_json, :callback => params[:callback]
   end
 
