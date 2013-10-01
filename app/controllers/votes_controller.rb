4a8db8e489287897ba131ff2b081e5de4dee3968
Apprentice <apprentice@dbc22.local>
2013-09-30 23:41:37 -0700

    Adds current user to vote controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,10 +10,12 @@ class VotesController < ApplicationController
   end
 
   def create
-    vote = Vote.new(voter_id: 1, voted_on_id: params[:id])
-    vote.opinion = "yes"
-    vote.save
-    User.first.votes << vote
+    user = User.find(params[:voter_id])
+    vote = Vote.create(voter_id: params[:voter_id], voted_on_id: params[:voted_on_id], opinion: params[:opinion])
+    user.votes << vote
+
+    # User.find(richie.id).votes.where(voted_on_id: dude.id).first.opinion == User.find(dude.id).votes.where(voted_on_id: richie.id).first.opinion
+  
     render json: vote.to_json
   end
 
