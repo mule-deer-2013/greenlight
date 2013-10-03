4dcdf199ddedffdd078064fc126207590e127426
Dev Bootcamp <Apprentice@dbc33.local>
2013-10-02 22:07:02 -0700

    deleted :password from validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   attr_accessible :name, :sex, :sex_preference, :age, :email, :tagline, :password, :photo, :longitude, :latitude
-  # validates_presence_of :name, :email, :sex, :sex_preference, :age, :password
+  # validates_presence_of :name, :email, :sex, :sex_preference, :age
   # validates_uniqueness_of :email
 
 
